a8395e4029a9ef503b8718a0d93a1ad0f3c8fe1c
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-10-25 12:47:30 +0100

    Add beds public/private data per population

--- data/population/parser_hospital_population.py
+++ data/population/parser_hospital_population.py
@@ -13,16 +13,22 @@ try:
     reader = csv.reader(f,delimiter=';')
     reader.next()
 
-    # Creates output file
-    of = open('hospitals-per-habitant.csv', 'wb')
-    wr = csv.writer(of, quoting=csv.QUOTE_ALL)
+    # Creates public output file
+    of_public = open('beds-publics-per-habitant.csv', 'wb')
+    public = csv.writer(of_public, quoting=csv.QUOTE_ALL)
+    public.writerow(['ID', 'PROVINCE', '2010', '2011', '2012', '2013', '2014'])
+
+    of_private = open('beds-private-per-habitant.csv', 'wb')
+    private = csv.writer(of_private, quoting=csv.QUOTE_ALL)
+    private.writerow(['ID', 'PROVINCE', '2010', '2011', '2012', '2013', '2014'])
 
     for row in reader:                                         # iterates the rows of the file in orders
         for num in range(INDEX_HOSPITAL,INDEX_POPULATION):   # to iterate trough hospital data
             year = math.floor((num - INDEX_HOSPITAL)/4)
             if row[num] != '':
-                row[num] = float(row[num]) / float(row[int(INDEX_POPULATION+year)])
-        row[INDEX_POPULATION:len(row)] = []     # remove columns with population
-        wr.writerow(row)
+                row[num] = 1000 * float(row[num]) / float(row[int(INDEX_POPULATION+year)])
+        #row[INDEX_POPULATION:len(row)] = []     # remove columns with population
+        public.writerow([row[2],row[3],row[21],row[17],row[13],row[9],row[5]])
+        private.writerow([row[2],row[3],row[23],row[19],row[15],row[11],row[7]])
 finally:
     f.close()      # closing
